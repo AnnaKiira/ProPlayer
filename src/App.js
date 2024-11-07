e36d80e0d70ed7099835982fb1fb08149b8ce779
User Name <annakirath@gmail.com>
2024-11-07 12:49:07 +0000

    set up buttons/a menu for selecting and playing different games. using useState hook to manage the state of the selected game

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
-//import {useState} from 'react'
-//import Hangman from './games/hangman/Hangman'
-//import Wordle from './games/wordle/Wordle'
-//import NumberGuesser from './games/number-guesser/NumberGuesser'
+import {useState} from 'react'
+import Hangman from './games/hangman/Hangman'
+import Wordle from './games/wordle/Wordle'
+import NumberGuesser from './games/number-guesser/NumberGuesser'
+
+function App() {
+  const [selectedGame, setSelectedGame] = useState(null);
+
+  return (
+    <div>
+      <h1>Games</h1>
+      <button onClick={() => setSelectedGame('Hangman')}>Play Hangman</button>
+      <button onClick={() => setSelectedGame('Wordle')}>Play Wordle</button>
+      <button onClick={() => setSelectedGame('NumberGuesser')}>Play Number Guesser</button>
+
+      {selectedGame === 'Hangman' && <Hangman />}
+      {selectedGame === 'Wordle' && <Wordle />}
+      {selectedGame === 'NumberGuesser' && <NumberGuesser />}
+    </div>
+  )
+}
+
+export default App;
