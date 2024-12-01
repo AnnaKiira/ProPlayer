2a7b779cacec20d75cc10301c805c9b38cf56d5c
User Name <annakirath@gmail.com>
2024-12-01 15:13:16 +0000

    build the grid in JSX so it renders in the browser. using grid.map() to iterate through each row in the grid array. using row.map() to iterate though each cell in the row, to render the letter. added classnames for styling and set up my wordle css as well to style the grid

--- src/games/wordle/Wordle.js
+++ src/games/wordle/Wordle.js
@@ -1,8 +1,9 @@
-/* import { useState, useMemo } from 'react' */
+import { useState } from 'react'
+import './Wordle.css'
 
 function Wordle() {
-    /* const [grid, setGrid] = useState(Array(6).fill('').map(() => Array(5).fill(''))) */
-    /* const [currentRow, setCurrenRow] = useState(0) */
+    const [grid, /* setGrid */] = useState(Array(6).fill('').map(() => Array(5).fill('')))
+    const [currentRow, /* setCurrenRow */] = useState(0)
     /* const words = useMemo(() => ['shade', 'haunt', 'audio', 'plane', 'canoe', 'raise', 'dream', 'title', 'panic'], []) */
     /* const [randomWord, setRandomWord] = useState(() => words[Math.floor(Math.random() * words.length)]) */
     /* const [gameOver, setGameOver] = useState(false) */
@@ -11,6 +12,24 @@ function Wordle() {
     return (
         <div className='wordleGame'>
             <h1>WORDLE</h1>
+
+            <div className="wordleGrid">
+                
+                {grid.map((row, rowIndex) => (
+                    <div key={rowIndex} className="row">
+                        {row.map((cell, columnIndex) => (
+                            <div
+                                key={columnIndex}
+                                className={`cell ${rowIndex === currentRow ? 'active' : ''}`}
+                            >
+                                {cell}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+
+            </div>
+
         </div>
     )
 }
