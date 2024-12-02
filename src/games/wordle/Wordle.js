7e471eafdd510d780674822ddcaf4e34667894df
User Name <annakirath@gmail.com>
2024-12-02 18:19:29 +0000

    build the keyboard in the wordle JSX, with previous experience building it in Hangman, it is very similar. Also set up my handleKeyClick function for the wordle game, so there is functionality when a user click a key/letter. This function will end if gameOver is true, if not it will keep executing the other if statement of currentGuess.length...... This if statement checks if the currentGuess 5 letters since all the wordle words in the game is 5 letters. The currentGuess will be updated with whatever keyvalue is inputted together with current guess. the setCurrentGuess updates the state now with the updatedGuess. the grid will also be updated. using the spread operator to create a shallow copy of the grid. the setGrid state is updated with the updatedGrid

--- src/games/wordle/Wordle.js
+++ src/games/wordle/Wordle.js
@@ -2,19 +2,34 @@ import { useState } from 'react'
 import './Wordle.css'
 
 function Wordle() {
-    const [grid, /* setGrid */] = useState(Array(6).fill('').map(() => Array(5).fill('')))
+    const [grid, setGrid] = useState(Array(6).fill('').map(() => Array(5).fill('')))
     const [currentRow, /* setCurrenRow */] = useState(0)
+    const [currentGuess, setCurrentGuess] = useState('')
     /* const words = useMemo(() => ['shade', 'haunt', 'audio', 'plane', 'canoe', 'raise', 'dream', 'title', 'panic'], []) */
     /* const [randomWord, setRandomWord] = useState(() => words[Math.floor(Math.random() * words.length)]) */
-    /* const [gameOver, setGameOver] = useState(false) */
+    const [gameOver, /* setGameOver */] = useState(false)
     /* const [isWinner, setIsWinner] = useState(false) */
+
+
+    const handleKeyClick = (keyvalue) => {
+        if (gameOver) return
+
+        if (currentGuess.length < 5) {
+            const updatedGuess = currentGuess + keyvalue
+            setCurrentGuess(updatedGuess)
+
+            const updatedGrid = [...grid]
+            updatedGrid[currentRow][currentGuess.length] = keyvalue
+            setGrid(updatedGrid)
+        }
+    }
 
     return (
         <div className='wordleGame'>
             <h1>WORDLE</h1>
 
             <div className="wordleGrid">
-                
+
                 {grid.map((row, rowIndex) => (
                     <div key={rowIndex} className="row">
                         {row.map((cell, columnIndex) => (
@@ -25,6 +40,20 @@ function Wordle() {
                                 {cell}
                             </div>
                         ))}
+                    </div>
+                ))}
+
+            </div>
+
+            <div className='keyboard'>
+
+                {['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'].map((keyvalue) => (
+                    <div
+                        key={keyvalue}
+                        className={`key ${gameOver ? 'disabled' : ''}`}
+                        onClick={() => handleKeyClick(keyvalue)}
+                    >
+                        {keyvalue.toUpperCase()}
                     </div>
                 ))}
 
